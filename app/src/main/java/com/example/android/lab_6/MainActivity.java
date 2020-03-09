99eedd379cb26802c34d22fc6e9c2766818f631f
Blaine <blaine@wiscoradio.com>
2020-03-09 01:13:34 -0500

    Milestone 1

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -3,11 +3,30 @@ package com.example.android.lab_6;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Bascom
+    private final LatLng mDestinationLatLng = new LatLng(43.0751935, -89.4044081);
+    private GoogleMap mMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            // Code to add point
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
